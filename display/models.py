e1cef9b2ed0f944555fb9c5eb9b56d7a619fb8c4
vineeth862 <65697022+vineeth862@users.noreply.github.com>
2021-06-08 12:45:10 +0530

    Update models.py

--- display/models.py
+++ display/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class whole_city(models.Model):
-    cities = models.TextField(max_length=100)
+    city_Names = models.TextField(max_length=100)
 
     def __str__(self):
-        return self.cities
+        return self.city_Names
